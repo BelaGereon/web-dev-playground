2c5f4f0cfa6ebc933267bf6c5fdde766d7e78e62
Bela Géréon <belagereon@gmx.de>
2023-10-13 16:28:10 +0200

    [PWP-19] :construction: add FloatingHeader component

---
+++ src/components/FloatingHeader/FloatingHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FloatingHeader.css";
+
+const FloatingHeader = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="header">Test Text 123 456</div>
+      </div>
+    </div>
+  );
+};
+
+export default FloatingHeader;
